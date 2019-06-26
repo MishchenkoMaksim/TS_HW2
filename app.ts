2fe91103f97a5894e0653c841b3c32c5b1927721
Maksim Mishchenko <max75583@gmail.com>
2019-06-26 15:54:23 +0300

    Добавлены два экземпляра для производного класса Chevrolet

--- app.ts
+++ app.ts
@@ -1,58 +1,83 @@
 abstract class Car {
-    constructor(public brand: string, protected volume: number) { }
+    constructor(protected brand: string) { }
 
-    abstract getVolume(): void;
+    abstract getBrand(): void;
 }
 
 class BMW extends Car{
     constructor(){
-        super('BMW', 3);
+        super('BMW');
     }
 
     public model: string;
-    public length: number;
+    public volume: number;
     public numbOfSeats: number;
     public getOpt() {
-        console.log(this.model + ' ' + this.length + ' ' + this.numbOfSeats);
+        console.log(this.model + ' ' + this.volume + ' ' + this.numbOfSeats);
     }
 
-    getVolume(): void{
+    getBrand(): void{
         console.log(this.volume);
     }
 }
 
 class Chevrolet extends Car{
     constructor(){
-        super('Chevrolet', 1.6);
+        super('Chevrolet');
     }
 
-    getVolume(): void{
-        console.log(this.volume);
+    public model: string;
+    public volume: number;
+    public numbOfSeats: number;
+    public getOpt() {
+        console.log(this.model + ' ' + this.volume + ' ' + this.numbOfSeats);
+    }
+
+    getBrand(): void{
+        console.log(this.brand);
     }
 }
 
 class Hyundai extends Car{
     constructor(){
-        super('Hyundai', 2.4);
+        super('Hyundai');
     }
 
-    getVolume(): void{
-        console.log(this.volume);
+    public model: string;
+    public volume: number;
+    public numbOfSeats: number;
+    public getOpt() {
+        console.log(this.model + ' ' + this.volume + ' ' + this.numbOfSeats);
+    }
+
+    getBrand(): void{
+        console.log(this.brand);
     }
 }
 
 let x5:BMW = new BMW();
 x5.model = 'X5';
-x5.length = 4.5;
+x5.volume = 3;
 x5.numbOfSeats = 5;
 x5.getOpt();
 
 let x6:BMW = new BMW();
 x6.model = 'X6';
-x6.length = 5;
+x6.volume = 4;
 x6.numbOfSeats = 6;
 x6.getOpt();
 
+let aveo:Chevrolet = new Chevrolet();
+aveo.model = 'Aveo';
+aveo.volume = 1.6;
+aveo.numbOfSeats = 5;
+aveo.getOpt();
+
+let camaro:Chevrolet = new Chevrolet();
+camaro.model = 'Camaro';
+camaro.volume = 5;
+camaro.numbOfSeats = 2;
+camaro.getOpt();
 
 
 
--- file.js/app.js
+++ file.js/app.js
@@ -1,44 +1,59 @@
 class Car {
-    constructor(brand, volume) {
+    constructor(brand) {
         this.brand = brand;
-        this.volume = volume;
     }
 }
 class BMW extends Car {
     constructor() {
-        super('BMW', 3);
+        super('BMW');
     }
     getOpt() {
-        console.log(this.model + ' ' + this.length + ' ' + this.numbOfSeats);
+        console.log(this.model + ' ' + this.volume + ' ' + this.numbOfSeats);
     }
-    getVolume() {
+    getBrand() {
         console.log(this.volume);
     }
 }
 class Chevrolet extends Car {
     constructor() {
-        super('Chevrolet', 1.6);
+        super('Chevrolet');
     }
-    getVolume() {
-        console.log(this.volume);
+    getOpt() {
+        console.log(this.model + ' ' + this.volume + ' ' + this.numbOfSeats);
+    }
+    getBrand() {
+        console.log(this.brand);
     }
 }
 class Hyundai extends Car {
     constructor() {
-        super('Hyundai', 2.4);
+        super('Hyundai');
     }
-    getVolume() {
-        console.log(this.volume);
+    getOpt() {
+        console.log(this.model + ' ' + this.volume + ' ' + this.numbOfSeats);
+    }
+    getBrand() {
+        console.log(this.brand);
     }
 }
 let x5 = new BMW();
 x5.model = 'X5';
-x5.length = 4.5;
+x5.volume = 3;
 x5.numbOfSeats = 5;
 x5.getOpt();
 let x6 = new BMW();
 x6.model = 'X6';
-x6.length = 5;
+x6.volume = 4;
 x6.numbOfSeats = 6;
 x6.getOpt();
+let aveo = new Chevrolet();
+aveo.model = 'Aveo';
+aveo.volume = 1.6;
+aveo.numbOfSeats = 5;
+aveo.getOpt();
+let camaro = new Chevrolet();
+camaro.model = 'Camaro';
+camaro.volume = 5;
+camaro.numbOfSeats = 2;
+camaro.getOpt();
 //# sourceMappingURL=app.js.map
